ff841f8d7b331038fcbe3fda5152679125531f3f
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-13 11:17:52 -0500

    finished setting up files for lecture. also finished introductory lesson on Servlets. making first commit

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,18 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+// Hey Tomcat, whenever someone requests the URL /hello, load and execute this servlet
+// The WebServlet annotation maps a URL to a servlet and its code
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<style> h1 {color : blue}</style><h1>Hello, world!</h1>");
     }
 }
