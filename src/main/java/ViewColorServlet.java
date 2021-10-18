34ac3313a53ea3e834f67421874bfaa02730796a
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-18 11:54:05 -0500

    refactored Pick and View color exercise. setAttribute is now used

--- src/main/java/ViewColorServlet.java
+++ src/main/java/ViewColorServlet.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 public class ViewColorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String color = request.getParameter("color");
+        request.setAttribute("color", color);
         request.getRequestDispatcher("viewcolor.jsp").forward(request, response);
-//        String color = request.getParameter("color");
-//        request.setAttribute("color", color);
     }
 }
--- src/main/webapp/outcome.jsp
+++ src/main/webapp/outcome.jsp
@@ -2,19 +2,13 @@
   Created by IntelliJ IDEA.
   User: schrader
   Date: 10/18/21
-  Time: 10:43 AM
+  Time: 11:23 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
-    <title>View Color</title>
-    <style>
-        body {
-            background-color: ${color};
-        }
-    </style>
+    <title>Outcome</title>
 </head>
 <body>
 
