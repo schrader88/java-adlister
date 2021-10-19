a1b101c99edb7d7a376ad7782827b7ba08bc1723
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-19 13:48:06 -0500

    added a bit to the LoginServlet in order to account for sad path

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -19,6 +19,8 @@ public class LoginServlet extends HttpServlet {
             String password = request.getParameter("password");
             if (username.equals("admin") && password.equals("password")) {
                 response.sendRedirect("/profile");
+            } else {
+                response.sendRedirect("/login");
             }
         }
     }
