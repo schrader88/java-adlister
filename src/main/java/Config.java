cac4517d9853724e41e8433271893b5f863d6614
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-21 16:47:11 -0500

    edit commit history

---
+++ src/main/java/Config.java
@@ -0,0 +1,13 @@
+public class Config {
+
+    public String getUrl() {
+        return "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+    }
+    public String getUser() {
+        return "schrader";
+    }
+    public String getPassword() {
+        return "william97";
+    }
+
+}
