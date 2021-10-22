ff5c475af54f4cddc9cf6b1ad51f3b229a86c136
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-22 16:53:56 -0500

    currently making progress in User Input exercises. on exercise three, register and RegisterServlet

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,15 +1,75 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
 
 public class MySQLUsersDao implements Users {
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword()
+            );
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+
+//    Not quite done with method below, must find out how to return User object:
+
+//    WORK IN PROGRESS     //
+
     @Override
     public User findByUsername(String username) {
+        try {
+            String safeSearchQuery = "SELECT * FROM users WHERE username = ?";
+
+            PreparedStatement ps = connection.prepareStatement(safeSearchQuery);
+
+            ps.setString(1, username);
+
+            ResultSet rs = ps.executeQuery();
+
+            return new User (
+                    rs.getLong("id"),
+                    rs.getString("username"),
+                    rs.getString("email"),
+                    rs.getString("password")
+            );
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Long insert(User user) {
-        return null;
+        try {
+            String insertQuery = String.format("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+
+            PreparedStatement ps = connection.prepareStatement(insertQuery, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            rs.next();
+
+            return rs.getLong(1);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
+        }
     }
 }
---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,25 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+<h1>Create an Account</h1>
+    <form action="/registration" method="POST">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input id="username" name="username" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input id="email" name="email" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input id="password" name="password" class="form-control" type="password">
+        </div>
+        <input type="submit" class="btn btn-primary btn-block" value="Create Account">
+    </form>
+</body>
+</html>
